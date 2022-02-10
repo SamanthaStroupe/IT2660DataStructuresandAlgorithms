84952cb760fe97655e3a58246b42e5713b68372f
Samantha Stroupe <86115778+SamanthaStroupe@users.noreply.github.com>
2022-02-10 18:07:12 -0500

    Add files via upload

---
+++ Assignment2/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+
+    private String name;
+    
+    public String getAnimalName()
+    {
+        return this.name;
+    } 
+    public String setAnimalName(String name)
+    {
+      return this.name; 
+    }
+   
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+}
